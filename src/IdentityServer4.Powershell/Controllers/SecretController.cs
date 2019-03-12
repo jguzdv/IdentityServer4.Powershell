f5175a0a63f48987d72cc9eb266ab2af0e7126c3
Glatzer, Thomas <glatzert@uni-mainz.de>
2019-03-12 12:30:48 +0100

    Remove some special chars, which are prone to problems

--- src/IdentityServer4.Powershell/Controllers/SecretController.cs
+++ src/IdentityServer4.Powershell/Controllers/SecretController.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace IdentityServer4.Powershell.Controllers
@@ -10,7 +10,7 @@ namespace IdentityServer4.Powershell.Controllers
             if (size <= 0 || size > 512)
                 throw new System.ArgumentOutOfRangeException(nameof(size), "Size should be 0 < x <= 512");
 
-            char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!\"§$%&/()=?[]\\`´+*~'#,;.-_".ToCharArray();
+            char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!/()[]\\+*~#,;.-_".ToCharArray();
 
             byte[] data = new byte[size];
             using (var crypto = new RNGCryptoServiceProvider())
